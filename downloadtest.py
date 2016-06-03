087352ddf19c3395b4fe60e1a6be16f216ca0566
Leo Durrant <Leo.durrant@outlook.com>
2016-06-03 22:06:32 +0100

    Removed junk files from the repo.

--- downloadtest.py
+++
@@ -1,7 +0,0 @@
-try:
-    import lbtlib
-except Exception:
-    print("Unable to load lbtlib. Please redownload this program or download lbtlib from http://github.com/ZanyLeonic/LeonicBinaryTool")
-    sys.exit(1)
-
-print(lbtlib.iolib.downloadfile("http://zanyleonic.github.io/LeonicBinaryTool/version.ver", "h"))
